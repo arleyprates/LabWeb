87ac6c113d0283416d90837697896399f2a4b2de
Arley Prates <arleyprates@gmail.com>
2015-04-23 12:15:40 -0300

    update to show info about contact

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,19 @@
+var i = 0;
+
+function mostraOpcoes(i) {
+
+	console.log("entrei!" + i);
+	var x = document.getElementById(i);
+	if (x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+	console.log(x);
+}
+
 function formDados() {
-	
+
 	var nome = document.getElementById('nome').value;
 	var sobrenome = document.getElementById('sobrenome').value;
 	var assunto = document.getElementById('assunto').value;
@@ -19,9 +33,16 @@ function formDados() {
 	*/
 
 	// elements to templat's form
+	var new_element_div_button = document.createElement("div");
 	var new_element_div = document.createElement("div");
+	//new_element_div.setAttribute("div", "contato");
+
 	var new_element_paragrafo = document.createElement("p");
+	new_element_paragrafo.setAttribute("onclick", "mostraOpcoes("+i+")");
+	
 	var new_element_texto = document.createElement("p");
+	new_element_texto.id = i++;
+	new_element_texto.setAttribute("style", "display: block;");
 
 	// textoNode with data's form
 	var name = document.createTextNode(nome);
@@ -40,6 +61,8 @@ function formDados() {
 	// append element's template with data's form in div main
 	destino.appendChild(new_element_div);
 	
+	destino.getElementById
+
 	//insertBefore
 
 
